78b50751837146def09d8e690da04dfe5e6e4e97
김윤혁 <kyhyeok@gmail.com>
2025-08-05 13:26:56 +0900

    조회수 어뷰징 방지 정책 설게 및 구현

--- service/view/src/main/java/hyeok/board/view/ViewApplication.java
+++ service/view/src/main/java/hyeok/board/view/ViewApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VieweApplication {
+public class ViewApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VieweApplication.class, args);
+        SpringApplication.run(ViewApplication.class, args);
     }
 }
--- service/view/src/main/java/hyeok/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/hyeok/board/view/service/ArticleViewService.java
@@ -1,17 +1,27 @@
 package hyeok.board.view.service;
 
 import hyeok.board.view.repository.ArticleViewCountRepository;
+import hyeok.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
     private final ArticleViewCountRepository articleViewCountRepository;
     private static final int BACK_UP_BATCH_SIZE = 100;
 
-    public Long increase(Long articleId, Long userId ) {
+    private static final Duration TTL = Duration.ofMinutes(10);
+
+    public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
 
         if (count % BACK_UP_BATCH_SIZE == 0) {
