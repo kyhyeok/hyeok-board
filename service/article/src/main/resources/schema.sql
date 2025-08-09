117f0f7493e43bc0f398e8d9ec8896e8c8c4cf89
김윤혁 <kyhyeok@gmail.com>
2025-08-09 16:04:25 +0900

    인기글 Producer 설계 - Transactional Messaging

--- service/article/src/main/resources/schema.sql
+++ service/article/src/main/resources/schema.sql
@@ -37,10 +37,21 @@ from (select article_id
 left join article on t.article_id = article.article_id;
 
 
-
-
 -- 게시글 수 테이블
 create table board_article_count (
     board_id bigint not null primary key,
     article_count bigint not null
 );
+
+
+-- outbox 테이블
+create table outbox (
+    outbox_id bigint not null primary key,
+    shard_key bigint not null,
+    event_type varchar(100) not null,
+    payload varchar(5000) not null,
+    created_at datetime not null
+);
+
+create index idx_shard_key_created_at on outbox(shard_key asc, created_at asc);
+-- 생성 10초 이후 조건 조회를 위한 인덱스
